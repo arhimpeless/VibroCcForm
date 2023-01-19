7ab8dc0bf58b759017d9fe6a8d2c6e558c0b3490
Andrey Markov <arherower@outlook.com>
2023-01-19 13:09:36 +0300

    One. 19,01,2023

--- VibrationCalculatorFormApp/Form1.cs
+++ VibrationCalculatorFormApp/Form1.cs
@@ -11,68 +11,326 @@ using VibroMath;
 public enum TextboxIsChangeable {
     Acceleration,
     Acceleration_dB,
+    Velocity,
+    Velocity_dB,
+    Displacement,
+    Displacement_dB,
+    Voltage,
+    Voltage_dB,
+    Frequency,
+    Sensitivity,
     NotTextbox
 }
 namespace VibrationCalculatorFormApp {
     public partial class Form1 : Form {
-        SignalParametrType AccType = SignalParametrType.RMS; // объяснить
+        SignalParametrType AccType = SignalParametrType.RMS;
+        SignalParametrType VelType = SignalParametrType.RMS;
+        SignalParametrType DisType = SignalParametrType.RMS;
+        SignalParametrType VoltType = SignalParametrType.RMS;
+        FrequencyType FreqType = FrequencyType.HZ;
         SensitivityType SenType = SensitivityType.mV_G;
         Freeze Freeze = Freeze.Acceleration;
-        
-
         public Form1() {
             InitializeComponent();
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
         }
-
         private void TSensitivity_KeyPress(object sender, KeyPressEventArgs e) {
            
         }
-
         private void TSensitivity_TextChanged(object sender, EventArgs e) {
-            Sensitivity sensitivity = new Sensitivity();
-            if (radSensitivityG.Checked) {
-                sensitivity.Set_mV_G(double.Parse(TSensitivity.Text));
-            }
-            if (radSensitivityM.Checked) {
-                sensitivity.Set_mV_MS2(double.Parse(TSensitivity.Text));
-            }
-            if (radAccSensFreq.Checked) {
-                VibroCalc.CalcAll(sensitivity, Freeze.Acceleration);
-            }
-            if (radVelSensFreq.Checked) {
-                VibroCalc.CalcAll(sensitivity, Freeze.Velocity);
-            }
-            if (radDispSensFreq.Checked) {
-                VibroCalc.CalcAll(sensitivity, Freeze.Displacement);
-            }
-
-            // После изменений в библиотеке и добавления полей для чекбоксов метод сокращается: 
             if (double.TryParse(TSensitivity.Text, out double result)) {
                 VibroCalc.CalcAll(new Sensitivity(result, SenType), Freeze);
             }
-            /// Добавить push textboxes    
+            PushAllTextboxes(TextboxIsChangeable.Sensitivity);
         }
-
         private void TFrequency_TextChanged(object sender, EventArgs e) {
-
+            if (double.TryParse(TFrequency.Text, out double result))
+            {
+                VibroCalc.CalcAll(new Frequency(result, FreqType), Freeze);
+            }
+            PushAllTextboxes(TextboxIsChangeable.Frequency);
         }
-
         private void TAcceleration_TextChanged(object sender, EventArgs e) {
-
+            if (double.TryParse(TAcceleration.Text, out double result))
+            {
+                VibroCalc.CalcAll(new Acceleration(result, AccType));
+            }
+            PushAllTextboxes(TextboxIsChangeable.Acceleration);
         }
-
         private void radAccelerationRMS_CheckedChanged(object sender, EventArgs e) {
-            /// Добавил рассказать
             if (radAccelerationRMS.Checked) {
                 AccType = SignalParametrType.RMS;
             }
-            /// Добавить push textboxes
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
         }
         private void PushAllTextboxes(TextboxIsChangeable isChangeable) {
-            /// добавил, рассказать
-            if (isChangeable != TextboxIsChangeable.Acceleration) {
-                TAcceleration.Text = VibroCalc.Acceleration.Get(AccType).ToString();
+            if (isChangeable != TextboxIsChangeable.Sensitivity)
+            {
+                TSensitivity.Access = Access.Blocked;
+                TSensitivity.Text = VibroCalc.Sensitivity.Get(SenType).ToString();
+                TSensitivity.Access = Access.UnLock;
             }
+            if (isChangeable != TextboxIsChangeable.Frequency) {
+                TFrequency.Access = Access.Blocked;
+                TFrequency.Text = VibroCalc.Frequency.Get(FreqType).ToString();
+                TFrequency.Access = Access.UnLock;
+            }
+                if (isChangeable != TextboxIsChangeable.Acceleration)
+            {
+                TAcceleration.Access = Access.Blocked;
+                TAcceleration.Text = VibroCalc.Acceleration.Get(AccType).ToString();
+                TAcceleration.Access = Access.UnLock;
+            }
+            if (isChangeable != TextboxIsChangeable.Acceleration_dB)
+            {
+                TAcceleration_dB.Access = Access.Blocked;
+                TAcceleration_dB.Text = VibroCalc.Acceleration.Get(SignalParametrType.dB).ToString();
+                TAcceleration_dB.Access = Access.UnLock;
+            }
+            if (isChangeable != TextboxIsChangeable.Velocity)
+            {
+                TVelocity.Access = Access.Blocked;
+                TVelocity.Text = VibroCalc.Velocity.Get(VelType).ToString();
+                TVelocity.Access = Access.UnLock;
+            }
+            if (isChangeable != TextboxIsChangeable.Velocity_dB)
+            {
+                TVelocity_dB.Access = Access.Blocked;
+                TVelocity_dB.Text = VibroCalc.Velocity.Get(SignalParametrType.dB).ToString();
+                TVelocity_dB.Access = Access.UnLock;
+            }
+            if (isChangeable != TextboxIsChangeable.Displacement)
+            {
+                TDisplacement.Access = Access.Blocked;
+                TDisplacement.Text = VibroCalc.Displacement.Get(DisType).ToString();
+                TDisplacement.Access = Access.UnLock;
+            }
+            if (isChangeable != TextboxIsChangeable.Displacement_dB)
+            {
+                TDisplacement_dB.Access = Access.Blocked;
+                TDisplacement_dB.Text = VibroCalc.Displacement.Get(SignalParametrType.dB).ToString();
+                TDisplacement_dB.Access = Access.UnLock;
+            }
+            if (isChangeable != TextboxIsChangeable.Voltage)
+            {
+                TVoltage.Access = Access.Blocked;
+                TVoltage.Text = VibroCalc.Voltage.Get(VoltType).ToString();
+                TVoltage.Access = Access.UnLock;
+            }
+            if (isChangeable != TextboxIsChangeable.Voltage_dB)
+            {
+                TVoltage_dB.Access = Access.Blocked;
+                TVoltage_dB.Text = VibroCalc.Voltage.Get(SignalParametrType.dB).ToString();
+                TVoltage_dB.Access = Access.UnLock;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void radVoltageRMS_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radVoltageRMS.Checked)
+            {
+                VoltType = SignalParametrType.RMS;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
+        }
+        private void radVoltagePik_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radVoltagePik.Checked)
+            {
+                VoltType = SignalParametrType.PIK;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
+        }
+        private void radVoltagePikPik_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radVoltagePikPik.Checked)
+            {
+                VoltType = SignalParametrType.PIK_PIK;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
+        }
+        private void radSensitivityG_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radSensitivityG.Checked)
+            {
+                SenType = SensitivityType.mV_G;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
+        }
+        private void radSensitivityM_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radSensitivityM.Checked)
+            {
+                SenType = SensitivityType.mV_MS2;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
+        }
+        private void radAccelerationPik_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radAccelerationPik.Checked)
+            {
+                AccType = SignalParametrType.PIK;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
+        }
+        private void radAccelerationPikPik_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radAccelerationPikPik.Checked)
+            {
+                AccType = SignalParametrType.PIK_PIK;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
+        }
+        private void radVelocityRMS_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radVelocityRMS.Checked)
+            {
+                VelType = SignalParametrType.RMS;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
+        }
+        private void radVelocityPik_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radVelocityPik.Checked)
+            {
+                VelType = SignalParametrType.PIK;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
+        }
+        private void radVelocityPikPik_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radVelocityPikPik.Checked)
+            {
+                VelType = SignalParametrType.PIK_PIK;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
+        }
+        private void radDisplacementRMS_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radDisplacementRMS.Checked)
+            {
+                DisType = SignalParametrType.RMS;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
+        }
+        private void radDisplacementPik_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radDisplacementPik.Checked)
+            {
+                DisType = SignalParametrType.PIK;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
+        }
+        private void radiDisplacementPikPik_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radiDisplacementPikPik.Checked)
+            {
+                DisType = SignalParametrType.PIK_PIK;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
+        }
+        private void radVoltSensFreq_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radVoltSensFreq.Checked)
+            {
+                Freeze = Freeze.Voltage;
+            }
+        }
+        private void radAccSensFreq_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radAccSensFreq.Checked)
+            {
+                Freeze = Freeze.Acceleration;
+            }
+        }
+        private void radVelSensFreq_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radVelSensFreq.Checked)
+            {
+                Freeze = Freeze.Velocity;
+            }
+        }
+        private void radDispSensFreq_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radDispSensFreq.Checked)
+            {
+                Freeze = Freeze.Displacement;
+            }
+        }
+        private void TVoltage_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(TVoltage.Text, out double result))
+            {
+                VibroCalc.CalcAll(new Voltage(result, VoltType));
+            }
+            PushAllTextboxes(TextboxIsChangeable.Voltage);
+        }
+        private void TVoltage_dB_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(TVoltage_dB.Text, out double result))
+            {
+                VibroCalc.CalcAll(new Voltage(result, SignalParametrType.dB));
+            }
+            PushAllTextboxes(TextboxIsChangeable.Voltage_dB);
+        }
+        private void TAcceleration_dB_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(TAcceleration_dB.Text, out double result))
+            {
+                VibroCalc.CalcAll(new Acceleration(result, SignalParametrType.dB));
+            }
+            PushAllTextboxes(TextboxIsChangeable.Acceleration_dB);
+        }
+        private void TDisplacement_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(TDisplacement.Text, out double result))
+            {
+                VibroCalc.CalcAll(new Displacement(result, AccType));
+            }
+            PushAllTextboxes(TextboxIsChangeable.Displacement);
+        }
+        private void TDisplacement_dB_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(TDisplacement_dB.Text, out double result))
+            {
+                VibroCalc.CalcAll(new Displacement(result, SignalParametrType.dB));
+            }
+            PushAllTextboxes(TextboxIsChangeable.Displacement_dB);
+        }
+        private void TVelocity_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(TVelocity.Text, out double result))
+            {
+                VibroCalc.CalcAll(new Velocity(result, AccType));
+            }
+            PushAllTextboxes(TextboxIsChangeable.Velocity);
+        }
+        private void TVelocity_dB_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(TVelocity_dB.Text, out double result))
+            {
+                VibroCalc.CalcAll(new Velocity(result, SignalParametrType.dB));
+            }
+            PushAllTextboxes(TextboxIsChangeable.Velocity_dB);
+        }
+        private void radFrequencyHw_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radFrequencyHw.Checked)
+            {
+                FreqType = FrequencyType.HZ;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
+        }
+        private void radFrequencyRPM_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radFrequencyRPM.Checked)
+            {
+                FreqType = FrequencyType.RPM;
+            }
+            PushAllTextboxes(TextboxIsChangeable.NotTextbox);
         }
     }
 }
